efe55ffd303d1c36fe3ccce051539e1d3acfd097
BOOM-08 <vasavabhumit4@gmail.com>
2025-09-11 02:08:33 +0530

    Add main.dart for Flutter starter app

---
+++ main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Language Learning App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Language Learning App'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to your Language Learning App!',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
